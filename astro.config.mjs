79a2ef9882377cc5f7222c3f57da33b565f6ffd2
Emanuel Hermosilla <hermosilla.emanuel811@gmail.com>
2024-03-02 15:49:17 -0300

    remove vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 })
